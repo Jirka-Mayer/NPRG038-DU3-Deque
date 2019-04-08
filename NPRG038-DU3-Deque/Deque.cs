ea2a7620177a7bc268c856ebbf871ec8cf7911b7
Jirka-Mayer <jirkamayer97@gmail.com>
2019-04-08 14:24:56 +0200

    Project setup

---
+++ NPRG038-DU3-Deque/Deque.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using NUnit.Framework;
+
+public class Deque<T>
+{
+    public Deque()
+    {
+    }
+}
+
+public static class DequeTest
+{
+
+}
+
+/**/
+
+[TestFixture]
+public class MyTests
+{
+    [Test]
+    public void exampleTest()
+    {
+        Assert.True(true);
+    }
+}
+
+/**/
---
+++ README.md
@@ -0,0 +1,6 @@
+Deque
+=====
+
+Veškerá implementace je v souboru `NPRG038-DU3-Deque/Deque.cs`.
+
+(v jednom souboru, aby se to dobře odevzdávalo do recodexu)
